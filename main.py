6ca458db14b8b094b9ca07c144906e0297bdd733
xmdn <smcartix@gmail.com>
2023-10-09 21:54:27 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import config
-import telebot, sys, time
+import telebot, sys, time, json
 from telebot import types
 
 # useful functions
@@ -7,22 +7,39 @@ def debug(text, level=1):
     if config.debug:
         print("#" + (" " * level) + text)
 
+def get_class_by_id(id):
+    global data
+    try:
+        result = data["people"][str(id)]
+    except:
+        result = None
+    return result
+
 # set up
 debug("Started setup")
 config.check_token()
 debug("Token checked!", 2)
 bot = telebot.TeleBot(config.token)
 debug("TeleBot initialized!", 2)
+debug("Fetching data.json", 2)
+datafile = open("data.json", encoding="utf-8")
+debug("Opened data.json as data file", 3)
+data = json.load(datafile)
+debug("Loaded data from data file to data", 3)
+datafile.close()
+debug("Data file closed", 3)
 
 # inline handler
 @bot.inline_handler(lambda query: len(query.query) == 0)
 def default_query(inline_query):
     try:
-        r = types.InlineQueryResultArticle('1', 'Д/З на завтра', types.InputTextMessageContent('Здесь будет написано дз на завтра'))
+        debug(str(inline_query))
+        # r = types.InlineQueryResultArticle('1', 'Д/З на завтра', types.InputTextMessageContent('Здесь будет написано дз на завтра'))
+        r = types.InlineQueryResultArticle('1', 'Ваш класс', types.InputTextMessageContent('ID вашего класса: ' + get_class_by_id(inline_query.from_user.id)))
         r1 = types.InlineQueryResultArticle('2', 'Расписание на сегодня', types.InputTextMessageContent('Здесь будет расписание на сегодня'))
         bot.answer_inline_query(inline_query.id, [r, r1])
     except Exception as e:
-        print(e)
+        print("exception in inline_handler: " + e)
 
 
 # main loop
@@ -38,3 +55,25 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print('\nExiting by user request.\n')
         sys.exit(0)
+
+'''
+ {
+    'id': '3615995219113418494', 
+    'from_user': {
+        'id': 841914494, 
+        'is_bot': False, 
+        'first_name': 'хмдн++', 
+        'username': 'xmdnusr', 
+        'last_name': None, 
+        'language_code': 'ru', 
+        'can_join_groups': None, 
+        'can_read_all_group_messages': None, 
+        'supports_inline_queries': None, 
+        'is_premium': None, 
+        'added_to_attachment_menu': None
+    }, 
+    'query': '', 
+    'offset': '', 
+    'chat_type': 'group', 
+    'location': None}
+'''
